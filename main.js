22215996c541d2c6a2c6ff2ecf1de4fc8b7f8cea
Leo Yu <abc1239054@gmail.com>
2021-12-01 22:00:28 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,394 @@
+import { Map, View } from 'ol'
+import { Group, Vector as VectorLayer, Tile } from 'ol/layer'
+import WMTSTileGrid from 'ol/tilegrid/WMTS'
+import { Fill, Stroke, Style, Text, Icon, Circle } from 'ol/style'
+import { getWidth, getTopLeft, getHeight, getBottomLeft } from 'ol/extent'
+import WMTS, { optionsFromCapabilities } from 'ol/source/WMTS'
+import WMTSCapabilities from 'ol/format/WMTSCapabilities'
+import { setupPanel, projection3826, projection3857 } from './modules/initialize'
+import { ScaleLine, defaults as defaultControls } from 'ol/control'
+import VectorSource from 'ol/source/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
+import Overlay from 'ol/Overlay'
+import { DEVICE_PIXEL_RATIO } from 'ol/has';
+
+const container = document.getElementById('popup')
+const content = document.getElementById('popup-content')
+const closer = document.getElementById('popup-closer')
+
+
+const overlay = new Overlay({
+    element: container,
+    autoPan: true,
+    autoPanAnimation: {
+        duration: 250,
+    },
+});
+
+closer.onclick = () => {
+    overlay.setPosition(undefined);
+    closer.blur();
+    return false;
+};
+
+
+const levelsEMAP = 20
+const levelsCaotun = 12
+
+const tileMatrixesEMAP = {
+    resolutions: new Array(levelsEMAP),
+    matrixIds: new Array(levelsEMAP),
+}
+
+const tileMatrixesCaotun = {
+    resolutions: new Array(levelsCaotun),
+    matrixIds: new Array(levelsCaotun),
+}
+
+const parser = new WMTSCapabilities()
+
+
+const projectionExtent3857 = projection3857.getExtent()
+const projectionExtent3826 = [
+    145633.028131307, 2257847.93310604, 349492.616446144, 2956700.67571342,
+]
+const sizeEMAP = getWidth(projectionExtent3857) / 256
+const sizeCaotun = getWidth(projectionExtent3826) / 256
+
+for (let z = 0; z < levelsEMAP; z++) {
+    // generate resolutions and matrixIds arrays for this WMTS
+    tileMatrixesEMAP.resolutions[z] = sizeEMAP / Math.pow(2, z)
+    tileMatrixesEMAP.matrixIds[z] = z
+}
+
+for (let z = 0; z < levelsCaotun; z++) {
+    // generate resolutions and matrixIds arrays for this WMTS
+    tileMatrixesCaotun.resolutions[z] = sizeCaotun / Math.pow(2, z)
+    tileMatrixesCaotun.matrixIds[z] = z
+}
+
+const commonOptions = {
+    matrixSet: "EPSG:3826",
+    format: "image/png",
+    projection: projection3826,
+    tileGrid: new WMTSTileGrid({
+        origin: getTopLeft(projectionExtent3826),
+        resolutions: tileMatrixesCaotun.resolutions,
+        matrixIds: tileMatrixesCaotun.matrixIds,
+    }),
+    serverType: "geoserver",
+    style: "default",
+}
+
+//讀取影像建立圖例樣式
+const getPattern = async (url) => {
+    const canvas = document.createElement('canvas')
+    const context = canvas.getContext('2d')
+
+
+    const img = new Image(128, 128)
+    img.src = url
+    await img.decode()
+    canvas.width = 128
+    canvas.height = 128
+    context.drawImage(img, 0, 0, canvas.width, canvas.height)
+    return context.createPattern(canvas, "repeat")
+}
+
+//將JSON轉換成樣式物件
+const getTextStyleFromJSON = (obj) => {
+
+    const textStyle = {}
+    const font = obj['font'] || '12px Calibri,sans-serif'
+    const fillOption = obj['fill'] || {
+        color: '#000'
+    }
+    const strokeOption = obj['stroke'] || {
+        color: '#fff',
+        width: 3
+    }
+
+    textStyle['font'] = font
+    textStyle['fill'] = new Fill(fillOption)
+    textStyle['stroke'] = new Stroke(strokeOption)
+
+    return textStyle
+}
+
+//從JSON建立樣式物件
+const getStyleFromJSON = async (styleJSON) => {
+    let pat = 'rgba(200, 200, 200, 0.8)'
+    let fillSrc = ''
+    if (styleJSON.hasOwnProperty('fill')) {
+        if (styleJSON.fill.hasOwnProperty('src')) {
+            pat = await getPattern(styleJSON.fill.src)
+            fillSrc = styleJSON.fill.src
+        } else if (styleJSON.fill.hasOwnProperty('color')) {
+            console.log('shit')
+            pat = styleJSON.fill.color
+        }
+    }
+    const fillOption = { color: pat }
+    const strokeOption = styleJSON.hasOwnProperty('stroke') ? styleJSON.stroke : {
+        color: '#644889',
+        width: 5,
+        lineJoin: 'bevel',
+        lineDash: [5, 10],
+    }
+
+    let iconOption = {}
+    let iconSrc = ''
+    if (styleJSON.hasOwnProperty('image')) {
+        iconOption = styleJSON.image
+        iconSrc = styleJSON.image.src
+    } else {
+        iconOption = {
+            crossOrigin: 'anonymous',
+            src: 'http://localhost:3001/api/legend/default_dot.png',
+            scale: 0.05
+
+
+        }
+        iconSrc = 'http://localhost:3001/api/legend/default_dot.png'
+    }
+    const textOption = styleJSON.hasOwnProperty('text') ? getTextStyleFromJSON(styleJSON['text']) : getTextStyleFromJSON({})
+
+
+
+    const style = new Style({
+        fill: new Fill(fillOption),
+        stroke: new Stroke(strokeOption),
+        image: new Icon(iconOption),
+        text: new Text(textOption),
+    })
+    style.legendSrc = fillSrc || iconSrc
+
+    return style
+}
+
+(async () => {
+    try {
+        console.time('Loading')
+
+        //載入向量圖層設定
+        const layerCollecttion = await fetch('http://localhost:3001/api/layers').then(res => res.json())
+        const availableLayers = layerCollecttion.layers
+        const vectorLayers = []
+        const fetchPromises = availableLayers.map(async (layer) => {
+            const geojson = await fetch(`http://localhost:3001/api/geojson/${layer.name}`).then(res => res.json())
+            const style = await getStyleFromJSON(layer.style)
+            geojson.style = style
+            return geojson
+        })
+
+        //載入向量圖層
+        const geojsons = await Promise.all(fetchPromises)
+
+        //初始化載入的圖層
+        for (const geojson of geojsons) {
+            let vectorLayer = new VectorLayer({
+                title: geojson.title,
+                name: geojson.name,
+                source: new VectorSource({
+                    features: new GeoJSON().readFeatures(geojson),
+                }),
+                style: geojson.style,
+                legendSrc: geojson.style.legendSrc,
+            })
+            vectorLayers.push(vectorLayer)
+
+
+        }
+
+        //載入WMTS服務XML
+        const wmtsHeader = await fetch('https://atlas.geo.ntnu.edu.tw/geoserver/gwc/service/wmts?REQUEST=GetCapabilities').then(res => res.text())
+        const result = parser.read(wmtsHeader)
+
+        const optionsCaotun = optionsFromCapabilities(result, {
+            layer: "mdgil:caotun",
+            crossOrigin: "anonymous",
+        })
+        const optionsChenggong = optionsFromCapabilities(result, {
+            layer: "mdgil:chunggong",
+            crossOrigin: "anonymous",
+        })
+
+        const optionsMuzha = optionsFromCapabilities(result, {
+            layer: "mdgil:muzha",
+            crossOrigin: "anonymous",
+        })
+
+        const caotun = new Tile({
+            source: new WMTS(optionsCaotun, commonOptions),
+            title: "草屯",
+            name: "caotun",
+        })
+
+        const chenggong = new Tile({
+            source: new WMTS(optionsChenggong, commonOptions),
+            title: "成功",
+            name: "chenggong",
+        })
+
+        const muzha = new Tile({
+            source: new WMTS(optionsMuzha, commonOptions),
+            title: "木柵",
+            name: "muzha",
+        })
+
+        const orthophoto = new Tile({
+            source: new WMTS({
+                url: "https://wmts.nlsc.gov.tw/wmts",
+                crossOrigin: "anonymous",
+                layer: "PHOTO2",
+                matrixSet: "EPSG:3857",
+                format: "image/png",
+                projection: projection3857,
+                tileGrid: new WMTSTileGrid({
+                    origin: getTopLeft(projectionExtent3857),
+                    resolutions: tileMatrixesEMAP.resolutions,
+                    matrixIds: tileMatrixesEMAP.matrixIds,
+                }),
+                style: "default",
+            }),
+            type: "base",
+            title: "通用版電子地圖正射影像",
+            visible: true,
+            name: 'orthophoto',
+        })
+
+        const emap = new Tile({
+            source: new WMTS({
+                url: "https://wmts.nlsc.gov.tw/wmts",
+                crossOrigin: "anonymous",
+                layer: "EMAP16",
+                matrixSet: "EPSG:3857",
+                format: "image/png",
+                projection: projection3857,
+                tileGrid: new WMTSTileGrid({
+                    origin: getTopLeft(projectionExtent3857),
+                    resolutions: tileMatrixesEMAP.resolutions,
+                    matrixIds: tileMatrixesEMAP.matrixIds,
+                }),
+                style: "default",
+            }),
+            type: "base",
+            title: "通用版電子地圖",
+            visible: false,
+            name: 'emap',
+        })
+
+        //建立地圖
+        const map = new Map({
+            controls: defaultControls().extend([new ScaleLine({
+                units: 'metric'
+            })]),
+            target: "mapWindow",
+            layers: [
+                new Group({
+                    name: 'basemap',
+                    title: "底圖",
+                    fold: "open",
+                    layers: [orthophoto, emap],
+                }),
+                new Group({
+                    title: "木柵",
+                    name: "muzha",
+                    fold: "open",
+                    layers: [muzha],
+                }),
+                new Group({
+                    title: "草屯",
+                    name: "caotun",
+                    fold: "open",
+                    layers: [caotun, ...vectorLayers],
+                }),
+                new Group({
+                    title: "成功",
+                    name: "changgong",
+                    fold: "open",
+                    layers: [chenggong],
+                }),
+            ],
+            overlays: [overlay],
+            view: new View({
+                center: [13464591, 2704238],
+                zoom: 8,
+                extent: [13000886, 2391185, 13864728, 2999458],
+            }),
+        })
+
+        //設定POP-UP視窗功能
+        map.on('click', (ev) => {
+
+            const feature = map.forEachFeatureAtPixel(ev.pixel,
+                (feature) => {
+                    return feature
+                }, {
+                hitTolerance: 7
+            })
+            console.log(feature)
+            if (feature) {
+                const rowInfo = {
+                    topo_name: 0,
+                    text: 1,
+                    image: 2
+                }
+                const properties = feature.getProperties()
+                delete properties['geometry']
+
+                const table = document.createElement('table')
+                let r, c
+                const keys = Object.keys(properties)
+                keys.forEach((k) => {
+                    r = table.insertRow(rowInfo[k])
+                    c = r.insertCell(0)
+                    switch (k) {
+                        case 'geometry':
+                            c.innerHTML = properties[k].getType()
+                            break
+                        case 'image':
+                            const img = document.createElement('img')
+                            img.src = `http://localhost:3001/api/img/${properties[k]}`
+                            img.className = 'feature-img'
+                            c.appendChild(img)
+                            break
+                        default:
+                            c.innerHTML = properties[k]
+                    }
+
+
+                })
+
+                if (content.firstElementChild) {
+                    content.replaceChild(table, content.firstElementChild)
+                }
+                else {
+                    content.appendChild(table)
+                }
+                const coordinate = ev.coordinate
+                overlay.setPosition(coordinate)
+            }
+        })
+
+        //初始化控制面板
+        setupPanel(map)
+
+        //停止載入畫面
+        map.once('postcompose', () => {
+            console.timeEnd('Loading')
+            const loadingPage = document.getElementById("loading-page")
+            loadingPage.style.display = 'none'
+        })
+
+    }
+    catch (err) {
+        const loadingPage = document.getElementById("loading-page")
+        loadingPage.style.display = 'none'
+        const infoDiv = document.getElementById("info")
+        infoDiv.innerHTML = `<p>Error: ${err}</p>`
+        console.log("Error:", err)
+    }
+
+
+})()
---
+++ modules/initialize.js
@@ -0,0 +1,315 @@
+import proj4 from 'proj4'
+import { register } from 'ol/proj/proj4'
+import { get as getProjection, transform, transformExtent, getPointResolution } from 'ol/proj'
+import html2canvas from 'html2canvas'
+import { jsPDF } from 'jspdf'
+import { Vector as VectorLayer, Tile } from 'ol/layer' 
+
+proj4.defs(
+  "EPSG:3826",
+  "+proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+)
+register(proj4)
+
+const projection3857 = getProjection("EPSG:3857")
+const projection3826 = getProjection("EPSG:3826")
+
+//顯示滑鼠位置TWD97座標
+const readMousePosition = (ev) => {
+  const coordinate = transform(ev.coordinate, projection3857, projection3826)
+  const coordinateReduced = coordinate.map(val => parseInt(val))
+  //console.log(coordinateReduced)
+  const mousePositionDiv = document.getElementById('mousePosition')
+  mousePositionDiv.innerHTML = `<span>&nbsp;TWD97座標:&nbsp;&nbsp;</span><span>${coordinateReduced[0]},&nbsp;&nbsp;${coordinateReduced[1]}</span>`
+
+}
+
+//使控制面板可拖動
+const dragElement = (elmnt) => {
+
+  const elementDrag = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  const dragMouseDown = (e) => {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  const closeDragElement = () => {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "Header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "Header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+}
+
+
+//更換底圖
+const switchBasemap = (baseLayers) => {
+  const checked = document.querySelector('[name=basemap]:checked')
+  baseLayers.forEach(layer => {
+    if (layer.get('name') === checked.value) {
+      layer.setVisible(true)
+    } else {
+      layer.setVisible(false)
+    }
+  })
+}
+
+//載入底圖設定選項
+const loadBaseSettings = (map) => {
+  const baseGroup = map.getLayerGroup().getLayers().getArray()[0]
+  const baseLayers = baseGroup.getLayersArray()
+  baseLayers.forEach(layer => {
+    const title = layer.get('title')
+    const name = layer.get('name')
+    const basemapSelection = document.getElementById('basemapSelection')
+    const el = document.createElement('div')
+    el.className = 'layer-item base'
+
+
+    const inputbox = document.createElement('input')
+    inputbox.type = 'radio'
+    inputbox.className = 'inputbox'
+    inputbox.id = name
+    inputbox.name = 'basemap'
+    inputbox.value = name
+    if (layer.getVisible()) {
+      inputbox.checked = true
+    }
+    inputbox.onchange = () => {
+      switchBasemap(baseLayers)
+    }
+    el.appendChild(inputbox)
+
+
+    const label = document.createElement('label')
+    label.for = name
+    label.className = 'layer-name'
+    label.appendChild(document.createTextNode(title))
+    el.appendChild(label)
+
+    basemapSelection.appendChild(el)
+  })
+
+}
+
+//載入地形特徵圖設定選項
+const loadLayerSettings = (map) => {
+  const featureGroups = map.getLayerGroup().getLayers().getArray().filter(layer => layer.get('name') !== 'basemap')
+  console.log(featureGroups)
+
+
+  featureGroups.forEach(group => {
+    const groupName = group.get('name')
+    console.log(groupName)
+    group.getLayersArray().forEach(layer => {
+      const title = layer.get('title')
+      const name = layer.get('name')
+      //console.log(layer.get('legendSrc'))
+      const featuremapSelection = document.getElementById('featuremapSelection')
+      const el = document.createElement('div')
+      
+
+      const inputbox = document.createElement('input')
+      inputbox.type = 'checkbox'
+      inputbox.id = name
+      inputbox.className = 'inputbox'
+      inputbox.checked = true
+      inputbox.onchange = () => {
+        layer.setVisible(inputbox.checked)
+      }
+      el.appendChild(inputbox)
+
+
+      const label = document.createElement('label')
+      label.for = name
+      label.title = '縮放至圖層範圍'
+      label.className = 'layer-name'
+      label.appendChild(document.createTextNode(title))
+      label.style.cursor = 'pointer'
+      label.onclick = () => {
+        let extent3826, extent3857
+        if (layer instanceof VectorLayer) {
+          extent3857 = layer.getSource().getExtent()
+        }
+        else {
+          extent3826 = layer.getSource().getTileGrid().getExtent()
+          extent3857 = transformExtent(extent3826, projection3826, projection3857)
+        }
+
+        map.getView().fit(extent3857, map.getSize())
+      }
+      el.appendChild(label)
+
+
+      const scrollBar = document.createElement('input')
+      scrollBar.className = 'opacity'
+      scrollBar.title = '透明度'
+      scrollBar.type = 'range'
+      scrollBar.min = 0
+      scrollBar.max = 1
+      scrollBar.step = 0.01
+      scrollBar.oninput = () => {
+        layer.setOpacity(parseFloat(scrollBar.value))
+      }
+      el.appendChild(scrollBar)
+
+
+      if (layer instanceof Tile) {
+        el.className = 'layer-item feature'
+        const collapseButton = document.createElement('div')
+        collapseButton.className = 'collapse-button'
+        collapseButton.appendChild(document.createTextNode('\u23F7'))
+        collapseButton.title = '展開圖層選項'
+        collapseButton.onclick = () => {
+          const items = document.querySelectorAll(`.layer-item.feature.${groupName}`)
+          console.log(items)
+          items.forEach(item => {
+            item.classList.toggle('active')
+            if(item.style.display === "flex") {
+              item.style.display = "none"
+              collapseButton.replaceChild(document.createTextNode('\u23F7'), collapseButton.firstChild)
+            } else {
+              item.style.display = "flex"
+              collapseButton.replaceChild(document.createTextNode('\u23F6'), collapseButton.firstChild)
+            }
+            
+          })
+        }
+        el.appendChild(collapseButton)
+        
+      } else {
+        el.className = `layer-item feature ${groupName}`
+        el.style.display = "none"
+        const collapseButton = document.createElement('div')
+        collapseButton.className = 'collapse-button'
+        collapseButton.appendChild(document.createTextNode('\u23F7'))
+        collapseButton.style.visibility = 'hidden'
+        el.appendChild(collapseButton)
+      }
+
+
+      featuremapSelection.appendChild(el)
+    })
+  })
+
+}
+
+//輸出成pdf目前有問題待解決
+const exportToPdf = (map) => {
+  //const loadingPage = document.getElementById("loading-page")
+  //loadingPage.style.display = 'flex'
+  const exportButton = document.getElementById('export')
+  exportButton.disabled = true
+  document.body.style.cursor = 'progress'
+  const dims = {
+    a0: [1189, 841],
+    a1: [841, 594],
+    a2: [594, 420],
+    a3: [420, 297],
+    a4: [297, 210],
+    a5: [210, 148],
+  }
+  //const format = document.getElementById('format').value;
+  //const resolution = document.getElementById('resolution').value;
+    //const scale = document.getElementById('scale').value;
+  //const dim = dims[format];
+  const format = 'a4'
+  const resolution = 300
+  const scale = 10
+  const dim = dims[format]
+  const width = Math.round((dim[0] * resolution) / 25.4)
+  const height = Math.round((dim[1] * resolution) / 25.4)
+  const viewResolution = map.getView().getResolution()
+  const size = map.getSize();
+  const scaleResolution =
+    scale /
+    getPointResolution(
+      map.getView().getProjection(),
+      resolution / 25.4,
+      map.getView().getCenter()
+    )
+  const exportOptions = {
+    useCORS: true,
+  }
+
+  map.once('rendercomplete', () => {
+    exportOptions.width = width
+    exportOptions.height = height
+    html2canvas(map.getViewport(), exportOptions).then((canvas) => {
+      const pdf = new jsPDF('landscape', undefined, format);
+      pdf.addImage(
+        canvas.toDataURL('image/jpeg'),
+        'PNG',
+        0,
+        0,
+        dim[0],
+        dim[1]
+      )
+      pdf.save('map.pdf')
+      // Reset original map size
+      //scaleLine.setDpi()
+      //map.getTargetElement().style.width = ''
+      //map.getTargetElement().style.height = ''
+      //map.updateSize()
+      map.setSize(size);
+      map.getView().setResolution(viewResolution);
+      exportButton.disabled = false
+      document.body.style.cursor = 'auto'
+      //const loadingPage = document.getElementById("loading-page")
+      //loadingPage.style.display = 'none'
+    })
+  })
+  //scaleLine.setDpi(resolution);
+  map.getTargetElement().style.width = width + 'px';
+  map.getTargetElement().style.height = height + 'px';
+  map.updateSize();
+
+
+  const printSize = [width, height];
+  //map.setSize(printSize);
+  const scaling = Math.min(width / size[0], height / size[1]);
+  console.log(`width: ${width} height: ${height} viewResolution: ${viewResolution} scaleResolution: ${scaleResolution} size: ${size} printSize: ${printSize}`)
+
+  map.getView().setResolution(viewResolution / scaling);
+  //map.getView().setResolution(scaleResolution);
+}
+//呼叫上面函式初始化控制面板
+const setupPanel = (map) => {
+  loadBaseSettings(map)
+  loadLayerSettings(map)
+  dragElement(document.getElementById('controlPanel'))
+  //const exportButton = document.getElementById('export')
+  //exportButton.onclick = () => exportToPdf(map)
+  map.on('pointermove', (ev) => {
+    readMousePosition(ev)
+  })
+
+}
+
+export { setupPanel, projection3826, projection3857 }
